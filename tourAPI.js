963e7fe350a1cb012430473b43a891cb39c97a5a
ting <h124702780@gmail.com>
2022-09-03 20:32:06 +0800

    API & portfolio page

--- tourAPI.js
+++ tourAPI.js
@@ -1,59 +1,70 @@
 
-    const url = "./data.json";
-    fetch(url)
-      .then((res) => {
-        const data = res.json();
-        return data;
-      })
-      .then((data) => {
-        console.log(data);
-        console.log(21)
-      });
-// $(document).ready(function() {
+    // const url = "restaurant_C_f.json";
+    // // https://ASIATING.github.io/API/02/js/data.json
+    // fetch(url)
+    //   .then((res) => {
+    //     const data = res.json();
+    //     return data;
+    //   })
+    //   .then((data) => {
+    //     console.log(data);
+    //     console.log(data.XML_Head.Infos.Info[0].Add)
+    //   });
 
-//     let showSearch = $('#searchpage');
-//     let showNoResult = $('#noresult');
+$(document).ready(function() {
+
+    let showSearch = $('#searchpage');
+    let showNoResult = $('#noresult');
+ 
 
 
-//     $('select#city').change(function() {
-//         $('.item').remove();
-//         let cityVal = $('select#city').val();
-//         console.log(12)
-//         $.ajax({
-//             type: 'GET',
-//             url: 'https://yunchians.github.io/API/02/js/data.json',
-//             dataType: 'json',
-//             success: function(data) {
-//                 // console.log('成功');
-//                 for (i = 0; i < data.length; i++) {
-//                     if (cityVal == data[i].鄉鎮市區) {
-//                         showNoResult.hide();
-//                         showSearch.show();
-//                         // console.log(data[i].鄉鎮市區);
-//                         let resultContent = `<div class="col-lg-12 item">
-//                                 <img src="./images/noimg.jpg">
-//                                 <div class="info">
-//                                     <div class="name"><span class="title">景點名稱：</span>${data[i].名稱}</div>
-//                                     <div class="address"><span class="title">地址：</span>${data[i].地址}</div>
-//                                     <div class="description"><span class="title">簡述：</span>${data[i].簡述}</div>
-//                                 </div>
-//                             </div>`
-//                         $('#searchpage > .row').append(resultContent);
+    $('select#city').change(function() {
+        $('.item').remove();
+        let cityVal = $('select#city').val();
+        console.log(12)
+        $.ajax({
+            type: 'GET',
+            url: 'restaurant_C_f.json',
+            dataType: 'json',
+            success: function(data) {
+                console.log('成功');
+                console.log(data)
+                console.log(data.XML_Head.Infos.Info.length)
+                for (i = 0; i < data.XML_Head.Infos.Info.length; i++) {
+                    if (cityVal == data.XML_Head.Infos.Info[i].Region) {
+                        console.log(data.XML_Head.Infos.Info[i])
+                        showNoResult.hide();
+                        showSearch.show();
+                        // console.log('good');
+                        //JSON內的圖片
+                         let imgdata =data.XML_Head.Infos.Info[i].Picture1;
+                        console.log(imgdata);
+                    //判斷JSON黨內有無圖片
+                        if(imgdata==""){
+                            imgdata = "./img/nopic.jpg";
+                        }
+                        let resultContent = `<div class="col-lg-12 item">
+                                <img src="${imgdata}">
+                                <div class="info">
+                                    <div class="name"><span class="title">景點名稱：</span>${data.XML_Head.Infos.Info[i].Name}</div>
+                                    <div class="address"><span class="title">地址：</span>${data.XML_Head.Infos.Info[i].Add}</div>
+                                    <div class="description"><span class="title">簡述：</span>${data.XML_Head.Infos.Info[i].Description}</div>
+                                </div>
+                            </div>`
+                        $('#searchpage > .row').append(resultContent);
 
-//                     } else if (cityVal == '請選擇') {
-//                         showSearch.hide();
-//                         showNoResult.show();
-//                         showNoResult.text('查無資料');
-//                         $('.item').remove();
-//                     }
-//                 }
-//                 // console.log(data);
+                    } else if (cityVal == '請選擇') {
+                        showSearch.hide();
+                        showNoResult.show();
+                        showNoResult.text('上方選擇縣市');
+                        $('.item').remove();
+                    }
+                }
 
-//             },
+            },
 
-//             error: function() {
-//                 // console.log('失敗');
-//             }
-//         })
-//     });
-// });
+            error: function() {
+            }
+        })
+    });
+});
